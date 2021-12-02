c64eb6a211a036e7909651679512e0ab94c1cf8a
deysan <ideysan@gmail.com>
2021-12-02 19:59:09 +0200

    Add style btn

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 const Counter = () => {
-  const count = 0;
+  const count = 1;
 
   const formatCounter = () => {
     return count === 0 ? 'empty' : count;
   };
 
+  const getBadgeClasses = () => {
+    let classes = 'badge m-2 ';
+    classes += count === 0 ? 'bg-warning' : 'bg-primary';
+    return classes;
+  };
+
   return (
     <>
-      <span className="badge bg-primary m-2">{formatCounter()}</span>
+      <span className={getBadgeClasses()}>{formatCounter()}</span>
       <button className="btn btn-primary btn-sm m-2">+</button>
     </>
   );
