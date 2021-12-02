f8d515f583538f5a3ee48fcb0b69b6ddf18985ed
deysan <ideysan@gmail.com>
2021-12-02 20:58:12 +0200

    Conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -26,19 +26,27 @@ const Counter = () => {
     setTags((prevState) => prevState.filter((tag) => tag !== id));
   };
 
+  const renderTags = () => {
+    return (
+      tags.length !== 0 &&
+      tags.map((tag) => (
+        <li
+          key={tag}
+          className="btn btn-primary m-2"
+          onClick={() => handleTagChange(tag)}
+        >
+          {tag}
+        </li>
+      ))
+    );
+  };
+
+  if (tags.length !== 0) {
+    return <ul>{renderTags()}</ul>;
+  }
+
   return (
     <>
-      <ul>
-        {tags.map((tag) => (
-          <li
-            key={tag}
-            className="btn btn-primary m-2"
-            onClick={() => handleTagChange(tag)}
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
       <span className={getBadgeClasses()}>{formatCounter()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
